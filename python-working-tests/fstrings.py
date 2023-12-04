fff83964d0f8b43a3b93f5f136cffb0aeefb83c3
Darvid <darvid.torres@gmail.com>
2023-12-04 07:29:34 -0600

    adding a few tests from previou exercises

---
+++ python-working-tests/fstrings.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+#s = 'bar'
+#print(f'foo.{s}.baz')
+#
+## f"newline: {ord('\n')}"
+#
+#s = 'bar'
+#print(f'foo\n{s}\nbaz')
+#
+#s = 'bar'
+#print(f'foo\n{s}\nbaz')
+
+#quantity = 6
+#item = 'bananas'
+#price = 1.74
+#print(f'{quantity} {item} cost ${price}')
+
+#x = 2
+#print(f'{x} cubed is {x**3}')
+
+#a = [1, 2 ,3]
+#b = {'foo': 1, 'bar': 2}
+#print(f'{a}')
+#print(f'{b}')
+
+#print(rf'foo\n')
+#print(fr'bar\n')
+
+#a = '\n'
+#print(f'New line: {a}')
+
+#print(-0.0)
+#print(-0.0 == 0)
+
+#x = 35.23
+#print(f'{x:.3e}')
+#print(f'{x:.3f}')
+#print(f'{x:.3g}')
+#print(f'{x:.3%}')
+
+#print("a")
+#print('''
+#Multi
+#line
+#''')
+
+word = 'Python'
+print(word[:6] + word[6:])
---
+++ python-working-tests/hello_world.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+Tasks
+0.Hello, print
+Write a Python script that prints exactly
+"Programming is like building a multilingual puzzle, followed by a new line.
+'''
+
+print(f'\"Programming is like building a multilingual puzzle')
+
+'''
+Complete this source code in order to print the integer stored in the variable number, followed by Battery street, followed by a new line.
+
+#!/usr/bin/python3
+number = 98
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+
+You can find the source code here
+The output of the script should be:
+the number, followed by Battery street,
+followed by a new line
+You are not allowed to cast the variable number into a string
+Your code must be 3 lines long
+You have to use f-strings tips
+'''
+number = 98
+print(f'{number} Battery street')
+
