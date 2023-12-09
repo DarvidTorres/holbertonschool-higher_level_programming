557c5be830af084428ffbd80b17a5ea5a6b838bf
Darvid <darvid.torres@gmail.com>
2023-12-09 14:28:02 -0600

    work in progress

--- python-tests/functions.py
+++ python-tests/functions.py
@@ -1,8 +1,48 @@
 #!/usr/bin/python3
+# def subtract(x, y):
+# 	"""Return the subtraction of passed args"""
+# 	r = x - y
+# 	return r
+
+# print(subtract(2, 3))
+# print(subtract(3, 1))
+
+# def default_args(x = 'Foo'):
+#     print(x)
+    
+# default_args()
+# default_args('Bar')
+
+# def keyword_args(first: )
+
+# def subtract(x, y):
+# 	"""Return the subtraction of passed args"""
+# 	r = x - y
+# 	return r
+
+# print(subtract(2, x = 3))
+
+# print(subtract(2, 3))
+# print(subtract(x = 2, y = 3))
+# print(subtract(y = 2, x = 3))
+
+# def subtract(x, y):
+# 	"""Return the subtraction of passed args"""
+# 	r = x - y
+# 	return r
+
+# print(subtract(x = 2, 3)
+
+# def subtract(x, y):
+# 	"""Return the subtraction of passed args"""
+# 	r = x - y
+# 	return r
+
+# print(subtract(2, x = 3))
+
 def subtract(x, y):
 	"""Return the subtraction of passed args"""
 	r = x - y
 	return r
 
-print(subtract(2, 3))
-print(subtract(3, 1))
+print(subtract(x = 2, 3))
