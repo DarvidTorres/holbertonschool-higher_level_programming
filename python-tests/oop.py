851128cdf922c2542db619a1d6c2d6702f1bd1c0
Darvid <darvid.torres@gmail.com>
2023-12-28 20:33:39 -0600

    adding code

--- python-tests/oop.py
+++ python-tests/oop.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python3
 class MyClass:
-	pass
+    """
+    A class that stores a single value and provides a method to display it.
 
-print(my_obj := MyClass())
+    Attributes
+    ----------
+    attr : any type
+        The value to be stored in the instance.
+
+    Methods
+    -------
+    show_value()
+        Prints the stored value to the console.
+    """
+
+    def __init__(self, value):
+        self.attr = value
+
+    def show_value(self):
+        print(self.attr)
+
+# Creating an instance of MyClass and calling show_value
+my_obj = MyClass('Value')
+
+my_obj.show_value()  # Calling the method attribute 'show_value'
